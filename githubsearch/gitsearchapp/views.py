923e9f0054c398bdc4c47d17e3b5652e05eca965
Evan Casey <evan.casey@certirx.com>
2017-06-01 13:53:05 -0400

    finish detail view

--- githubsearch/gitsearchapp/views.py
+++ githubsearch/gitsearchapp/views.py
@@ -11,7 +11,6 @@ import json
 def index(request):
     parsedData = []
     if request.method == 'POST':
-        print request.POST
         keyword = request.POST.get('keyword')
         req = requests.get('https://jobs.github.com/positions.json?description=' + keyword + '&location=san+francisco')
         jsonList = []
@@ -25,9 +24,21 @@ def index(request):
                 jobData['id'] = data['id']
                 parsedData.append(jobData)
 
-    return render(request, 'gitsearchapp/profile.html', {'data': parsedData})
+    return render(request, 'gitsearchapp/index.html', {'data': parsedData})
 
 def detail(request):
+    parsedData = []
     job_id = request.GET.get('id')
-    req = requests.get('https://jobs.github.com/positions/' + job_id + '.json')
-    return HttpResponse(req)
+    req = requests.get('https://jobs.github.com/positions/' + job_id + '.json?markdown=true')
+
+    jsonList = []
+    jsonList.append(json.loads(req.content))
+    jobData = {}
+    for data in jsonList:
+        jobData['title'] = data['title']
+        jobData['company'] = data['company']
+        jobData['description'] = data['description']
+        jobData['company_url'] = data['company_url']
+
+    parsedData.append(jobData)
+    return render(request, 'gitsearchapp/detail.html', {'data': parsedData})
