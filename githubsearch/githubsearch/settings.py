a240221a0f76e536ecb66f86df00e352289e8543
Evan Casey <evan.casey@certirx.com>
2017-06-01 14:01:07 -0400

    Added secret key back
    Not ideal, and would not do this in production environment, but no data is at stake with this personal project. Program requires this to run

--- githubsearch/githubsearch/settings.py
+++ githubsearch/githubsearch/settings.py
@@ -20,6 +20,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/1.11/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
+SECRET_KEY = 'r)u=5z2^ei$=(x^xz7hz-++pyjw87nhoa=__i=^y#%*lbn$0le'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
